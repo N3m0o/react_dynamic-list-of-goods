193f53903eedb19343ab8b1897e2b7898483fd75
Karen Behlarian <Gladiatorkaren@gmail.com>
2025-10-22 21:23:27 +0300

    add task solution

--- src/App.tsx
+++ src/App.tsx
@@ -4,45 +4,61 @@ import { GoodsList } from './GoodsList';
 import { Good } from './types/Good';
 import { getAll, get5First, getRedGoods } from './api/goods';
 
-
 export const App: React.FC = () => {
   const [goods, setGoods] = useState<Good[]>([]);
+  const [error, setError] = useState<string | null>(null);
 
   const handleLoadAll = () => {
     getAll().then(data => {
       setGoods(data);
-    });
-  }
+    }).catch(() => {
+      setError('Failed to load all goods')
+    })
+  };
 
   const handleLoadFive = () => {
     get5First().then(data => {
       setGoods(data);
+    }).catch(() => {
+      setError('Failed to load five goods')
     });
-  }
+  };
 
   const handleLoadRed = () => {
     getRedGoods().then(data => {
       setGoods(data);
-    })
-  }
+    }).catch(() => {
+      setError('Failed to load red goods 😢')
+    });
+  };
 
   return (
     <div className="App">
-    <h1>Dynamic list of Goods</h1>
+      <h1>Dynamic list of Goods</h1>
 
-    <button type="button" data-cy="all-button" onClick={handleLoadAll}>
-      Load all goods
-    </button>
+      <button type="button" data-cy="all-button" onClick={handleLoadAll}>
+        Load all goods
+      </button>
 
-    <button type="button" data-cy="first-five-button" onClick={handleLoadFive}>
-      Load 5 first goods
-    </button>
+      <button
+        type="button"
+        data-cy="first-five-button"
+        onClick={handleLoadFive}
+      >
+        Load 5 first goods
+      </button>
 
-    <button type="button" data-cy="red-button" onClick={handleLoadRed}>
-      Load red goods
-    </button>
+      <button type="button" data-cy="red-button" onClick={handleLoadRed}>
+        Load red goods
+      </button>
 
-    <GoodsList goods={goods} />
-  </div>
-  )
+      {error && (
+      <p className='error-message'> 
+      {error}
+      </p>
+      ) }
+
+      <GoodsList goods={goods} />
+    </div>
+  );
 };
